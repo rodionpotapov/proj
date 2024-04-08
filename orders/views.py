3519f0bc26f98c8f454dff1f088aca3753396fc6
rodion <144445240+rodionpotapov@users.noreply.github.com>
2024-04-09 00:37:01 +0300

    Add files via upload

---
+++ orders/views.py
@@ -0,0 +1,71 @@
+from email import message
+from django.contrib import messages
+from django.db import transaction
+from django.forms import ValidationError
+from django.shortcuts import redirect, render
+
+from carts.models import Cart
+from orders.forms import CreateOrderForm
+from orders.models import Order, OrderItem
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def create_order(request):
+
+    if request.method == "POST":
+        form = CreateOrderForm(data = request.POST)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    user = request.user
+                    cart_items = Cart.objects.filter(user=user)
+
+                    if cart_items.exists():
+                        # создать заказ
+                        order = Order.objects.create(
+                            user = user,
+                            phone_number = form.cleaned_data['phone_number'],
+                            requires_delivery = form.cleaned_data['requires_delivery'],
+                            payment_on_get = form.cleaned_data['payment_on_get']
+                        )
+                        # ссоздать заказанные товары
+                        for cart_item in cart_items:
+                            product = cart_item.product
+                            name = cart_item.product.name
+                            price = cart_item.product.sell_price()
+                            quantity = cart_item.quantity
+
+                            if product.quantity < quantity:
+                                raise ValidationError(f'Недостаточное количество товара {name} на складе / В наличии - {product.quantity}')
+                            
+                            OrderItem.objects.create(
+                                order = order ,
+                                product = product ,
+                                name = name ,
+                                price = price ,
+                                quantity = quantity,
+                            )
+                            product.quantity -= quantity
+                            product.save()
+
+                        # очистить корзину пользователя 
+                        cart_items.delete()
+                        messages.success(request , 'Заказ оформлен')
+                        return redirect('user:profile')
+            except ValidationError as e:
+                messages.success(request , str(e))
+                return redirect('cart:order')
+
+    else:
+        initial = {
+            'first_name' : request.user.first_name,
+            'last_name' : request.user.last_name,
+            }
+        form = CreateOrderForm(initial = initial)
+
+    context = {
+        'title' : 'Home - оформление заказа',
+        'form' : form ,
+        'order' : True,
+    }
+    return render(request , 'orders/create_order.html' , context = context)
---
+++ users/views.py
@@ -0,0 +1,111 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth , messages
+from django.db.models import Prefetch
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from carts.models import Cart
+from orders.models import Order, OrderItem
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
+
+# Create your views here.
+def login(request):
+    
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username , password = password)
+
+
+            session_key = request.session.session_key # назначаем сессионный ключ
+
+            if user:
+                auth.login(request, user)
+                messages.success(request , f"{username}, Вы вошли в аккаунт!")
+
+                if session_key:
+                    Cart.objects.filter(session_key = session_key).update(user=user) # фильтрация по сессионному ключу
+
+                redirect_page = request.POST.get('next' , None)
+
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title' : 'Home -  Авторизация' , 
+        'form' : form
+    }
+
+    return render(request,'users/login.html',context)
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+
+            session_key = request.session.session_key  # назначаем сессионный ключ
+
+            user = form.instance
+            auth.login(request, user)
+            
+            if session_key:
+                    Cart.objects.filter(session_key = session_key).update(user=user) # фильтрация по сессионному ключу
+
+            messages.success(request , f"{user.username}, Вы вошли в аккаунт!")
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
+
+    context = {
+        'title' : 'Home - Регистрация',
+        'form' : form 
+    }
+
+    return render(request , 'users/registration.html' , context)
+
+
+@login_required
+def profile(request):
+
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST , instance = request.user , files = request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request , "Профиль успешно обновлен!")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance = request.user)
+
+    orders = Order.objects.filter(user=request.user).prefetch_related(Prefetch("orderitem_set",queryset=OrderItem.objects.select_related("product"),)).order_by("-id")
+
+    context = {
+        'title' : 'Home - Профиль',
+        'form' : form,
+        'orders' : orders
+    }
+    return render(request , 'users/profile.html' ,context)
+
+
+def users_cart(request):
+    return render(request , 'users/users_cart.html')
+
+
+
+
+
+@login_required
+def logout(request):
+    messages.success(request , f"{request.user.username}, Вы вышли из аккаунта!")
+    auth.logout(request)
+
+    return redirect(reverse('main:index'))
